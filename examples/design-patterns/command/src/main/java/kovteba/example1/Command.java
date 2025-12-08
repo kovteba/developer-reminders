b8be219e4358ae0931f86c8e57ae5101442d6a0d
Kovteba Dima <kovteba@gmail.com>
2025-12-08 12:29:41 +0100

    ВК-21 Command Pattern

---
+++ examples/design-patterns/command/src/main/java/kovteba/example1/Command.java
@@ -0,0 +1,7 @@
+package kovteba.example1;
+
+// tag::Command[]
+public interface Command {
+    void execute();
+}
+// end::Command[]
---
+++ examples/design-patterns/command/src/main/java/kovteba/example1/Light.java
@@ -0,0 +1,14 @@
+package kovteba.example1;
+
+// tag::Light[]
+public class Light {
+
+    public void turnOn() {
+        System.out.println("💡 Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("💡 Light is OFF");
+    }
+}
+// end::Light[]
